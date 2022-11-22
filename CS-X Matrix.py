28c33547bf71136de8c00f70e8985dce259af9c7
andyp714 <85464699+andyp714@users.noreply.github.com>
2022-11-22 18:13:06 -0500

    Initial commit

---
+++ CS-X Matrix.py
@@ -0,0 +1,19 @@
+print("hello world")
+
+class Matrix():
+    def __init__(self, rowNumber, columnNumber, array):
+        self.rowNumber = rowNumber
+        self.columnNumber = columnNumber
+        self.array = array
+
+    def printMatrix(self):
+        for row in self.array:
+            for item in row:
+                print(item, end=' ')
+
+            print('')
+
+
+
+m1 = Matrix(3,2, [[2,1],[1,2], [0,1]])
+m1.printMatrix()
