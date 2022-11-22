f1756a8cf00374283b656e073be8244bb74c3417
andyp714 <85464699+andyp714@users.noreply.github.com>
2022-11-22 18:43:18 -0500

    Update CS-X Matrix.py

--- CS-X Matrix.py
+++ CS-X Matrix.py
@@ -1,9 +1,9 @@
 print("hello world")
 
 class Matrix():
-    def __init__(self, rowNumber, columnNumber, matrixArray):
-        self.rowNumber = rowNumber
-        self.columnNumber = columnNumber
+    def __init__(self, rowSize, columnSize, matrixArray):
+        self.rowSize = rowSize
+        self.columnSize = columnSize
         self.array = matrixArray
 
     def printMatrix(self):
@@ -13,8 +13,30 @@ class Matrix():
 
             print('')
 
+    def plusMatrix(self, m2):
+        if self.rowSize == m2.rowSize and self.columnSize == m2.columnSize:
+            for indexRow, row in enumerate(self.array):
+                #print("ROW", indexRow, ":", row)
+                for indexCol, item in enumerate(row):
+                    self.array[indexRow][indexCol] += m2.array[indexRow][indexCol]
+        else:
+            print("Not Same Size, Addition can't be performed")
+
+    def scalarTimesRow(self, scalar, rowTarget):
+        for index, item in enumerate(self.array[rowTarget-1]):
+            self.array[rowTarget-1][index] *= scalar
 
 
-m1 = Matrix(3,2, [[2,1],[1,2], [0,1]])
+
+
+m1 = Matrix(3,2, [[5,6],
+                  [4,3],
+                  [1,2]])
+m2 = Matrix(3,2, [[3,3],
+                  [3,3],
+                  [1,4]])
 m1.printMatrix()
-print("test")
+print('')
+m1.scalarTimesRow(2,3)
+m1.printMatrix()
+
