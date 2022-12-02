a7e49140d8174d79360516d64380397bea403da1
andyp714 <85464699+andyp714@users.noreply.github.com>
2022-12-01 23:45:34 -0500

    Update CS-X Matrix.py

--- CS-X Matrix.py
+++ CS-X Matrix.py
@@ -27,22 +27,43 @@ class Matrix():
 
 
     def matrixMultiply(self, m2):
+        newArray = []
+        #sets up a blank array
         if self.columnSize == m2.rowSize:
-            for indexRow, row in enumerate(self.array):
-                for indexCol, col in enumerate(row):
-                    pass
+            #checks if able to multiply
+            #New size of Matrix is m1 Row by m2 column, so the for loops are looping for that amount
+            for indexRow in range(self.rowSize):
+                newRow = []
+                #sets up a blank list
+                for indexCol in range(m2.columnSize):
+                    newNum = 0
+                    #sets up a blank number to keep track of the sum
+                    for index, item in enumerate(m1.array[indexRow]):
+                        #for each new number in a list, you have to sum n products, where n how many columns there are in m1. So this loops n times, adding the products together
+                        newNum += m1.array[indexRow][index] * [i[indexCol] for i in m2.array][index]
+                        #adds the product of the first number of m1 row and the first number of m2 column. which m1 row depends on the row of the new matrix. which m2 column depends on the column of the new matrix
+                        #the [i[indexCol] for i in m2.array] is a list of the column. First it takes the row, then takes a specifc index of row corresponding to the column. It then takes all the results from the for loop and makes it a list
+                    newRow.append(newNum)
+                    #adds the sum, or the result to a list
+
+                newArray.append(newRow)
+                #appends each newrow to this new matrix
+            m1.array = newArray
+
+
+
         else:
             print("Unable to multiply: Column size does not match row size. ")
 
 
 
 
-m1 = Matrix(3,2, [[5,6],
-                  [4,3],
-                  [1,2]])
-m2 = Matrix(3,2, [[3,3],
-                  [3,3],
-                  [1,4]])
+m1 = Matrix(2,2, [[1,2],
+                  [4,5]])
+m2 = Matrix(3,2, [[10,11],
+                  [20,21],
+                  [30,31]])
+#m1.printMatrix()
+m1.matrixMultiply(m1)
 m1.printMatrix()
-m1.matrixMultiply(m2)
 
